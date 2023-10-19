b0b75bc18efa8529c3376f974d01a517db01c792
Thomas Wright <thomas.wright@caliqon.com>
2023-10-19 14:09:15 +0100

    added repos to forms

---
+++ source/features/Buddy.Features.Forms/Repositories/QuestionRepository.cs
@@ -0,0 +1,12 @@
+﻿using Buddy.Features.Forms.Entities;
+using Buddy.Infrastructure.EntityFramework.Repositories;
+using Microsoft.Extensions.Logging;
+
+namespace Buddy.Features.Forms.Repositories;
+
+public class QuestionRepository : RepositoryBase<FormsDbContext, SurveyQuestion, Guid>
+{
+    public QuestionRepository(FormsDbContext context, ILogger<QuestionRepository> logger) : base(context, logger)
+    {
+    }
+}
---
+++ source/features/Buddy.Features.Forms/Repositories/SurveyRepository.cs
@@ -0,0 +1,12 @@
+﻿using Buddy.Features.Forms.Entities;
+using Buddy.Infrastructure.EntityFramework.Repositories;
+using Microsoft.Extensions.Logging;
+
+namespace Buddy.Features.Forms.Repositories;
+
+public class SurveyRepository : RepositoryBase<FormsDbContext, SurveyForm, Guid>
+{
+    public SurveyRepository(FormsDbContext context, ILogger<SurveyRepository> logger) : base(context, logger)
+    {
+    }
+}
