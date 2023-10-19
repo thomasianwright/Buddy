ed78d2798d94f48f3a67b7ab5f94e8a705fac959
Thomas Wright <thomas.wright@caliqon.com>
2023-10-19 15:08:11 +0100

    added basics

---
+++ source/webapp/Buddy.WebApp/Pages/Builder/Index.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Buddy.WebApp.Pages.Builder;
+
+public partial class Index : ComponentBase
+{
+    [Parameter]
+    public Guid Id { get; set; } = Guid.Empty;
+}
